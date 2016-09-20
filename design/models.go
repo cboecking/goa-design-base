7c06ce13de1bc3728dd28f2f6dfb5b0e82c1fb59
cboecking <chuck@chuboe.com>
2016-09-20 15:48:46 -0500

    Create models.go

---
+++ design/models.go
@@ -0,0 +1,22 @@
+package design
+
+import (
+	"github.com/goadesign/gorma"
+	. "github.com/goadesign/gorma/dsl"
+)
+
+var _ = StorageGroup("XSTORGROUP_U", func() {
+	Description("This is the global storage group")
+	Store("postgres", gorma.Postgres, func() {
+		Description("This is the Postgres relational store")
+		Model("XSTORMODEL_U", func() {
+			RendersTo(XSTORGROUP_L.XSTORMODEL_U)
+			Description("XSTORGROUP_U XSTORMODEL_U")
+			Field("id", gorma.Integer, func() {
+				PrimaryKey()
+			})
+			Field("name", gorma.String)
+		})
+
+	})
+})
