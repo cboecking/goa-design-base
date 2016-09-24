227c01753a2652169f4d3855a995dec0a03c4c15
Chuck Boecking <chuck@chuboe.com>
2016-09-24 06:56:51 -0500

    forgot to add these files to the previous commit

--- util/new_goa.sh
+++ util/new_goa.sh
@@ -19,8 +19,9 @@ fi
 mkdir -p $GOA_DESIGN_DEST
 cp $GOA_DESIGN_SOURCE/* $GOA_DESIGN_DEST/.
 
-#set proper case
+#set variables with proper case
 XAPI_NAME=${MOEBOE_PROP_GOA_API_NAME,,}
+XAPI_PORT=$MOEBOE_PROP_GOA_API_PORT
 XRESOURCE_L=${MOEBOE_PROP_GOA_RESOURCE_NAME,,}
 XRESOURCE_U=${XRESOURCE_L^}
 XMEDIATYPE_L=${MOEBOE_PROP_GOA_MEDIA_TYPE_NAME,,}
@@ -54,5 +55,19 @@ echo "** 2) goagen bootstrap -d $GOA_DESIGN_DEST_REL"
 echo "** 4) goagen --design=$GOA_DESIGN_DEST_REL gen --pkg-path=github.com/goadesign/gorma"
 echo "** 5) go build ."
 echo "** 6) ./$MOEBOE_PROP_GOA_PACKAGE_NAME"
-echo "** 7) issue this command from another shell: http http://localhost:8081/$XAPI_NAME/${XRESOURCE_L}s"
+echo "** NOTE: 	This program will execute; however, it will not persist any accounts."
+echo "** 	You need to update the $XRESOURCE_L.go controller."
+echo "**	Here is an example: https://github.com/goadesign/gorma-cellar/blob/master/account.go"
+echo "**	If using a db, update main.go with the db details:" 
+echo "**	Here is an example: https://github.com/goadesign/gorma-cellar/blob/master/main.go"
+echo "** 7) use the following commands to interact with the service (commands begins with http):"
+echo "****** LIST:    http http://localhost:8081/$XAPI_NAME/${XRESOURCE_L}s"
+echo "****** CREATE:  http POST http://localhost:8081/$XAPI_NAME/${XRESOURCE_L}s name=chuck1"
+echo "****** LIST:    http http://localhost:8081/$XAPI_NAME/${XRESOURCE_L}s"
+echo "****** CREATE:  http POST http://localhost:8081/$XAPI_NAME/${XRESOURCE_L}s name=chuck2"
+echo "****** LIST:    http http://localhost:8081/$XAPI_NAME/${XRESOURCE_L}s"
+echo "****** SHOW:    http http://localhost:8081/$XAPI_NAME/${XRESOURCE_L}s/2"
+echo "****** DELETE:  http DELETE http://localhost:8081/$XAPI_NAME/${XRESOURCE_L}s/2"
+echo "****** UPDATE:  http PUT http://localhost:8081/$XAPI_NAME/${XRESOURCE_L}s/1 name=chuckles"
+echo "****** LIST:    http http://localhost:8081/$XAPI_NAME/${XRESOURCE_L}s"
 echo "***************************"
